7d854f92eb1192a7cef8197f6c3515cb20a1b199
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2020-08-01 23:51:50 -0300

    Proyectos extension now has design and content

--- src/app/pages/proyectos-extension/proyectos-extension.page.ts
+++ src/app/pages/proyectos-extension/proyectos-extension.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-proyectos-extension',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectosExtensionPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+
+  // Modulos para navegar a la pagina correspondiente de la tarjeta
+  gotoEscuelaMedia(): void {
+    this.router.navigateByUrl('/proyectos-extension/escuela-media');
+  }
+
+  gotoAgentesRobots(): void {
+    this.router.navigateByUrl('/proyectos-extension/agentes-robots');
+  }
+
+  gotoTorneoProgramacion(): void {
+    this.router.navigateByUrl('/proyectos-extension/torneo-programacion');
+  }
+
+  gotoRealidadVirtual_RealidadAumentada(): void {
+    this.router.navigateByUrl('/proyectos-extension/realidad-aumentada_realidad-virtual');
+  }
+
 
 }
