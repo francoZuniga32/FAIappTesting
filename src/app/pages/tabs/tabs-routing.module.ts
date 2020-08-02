cd944a02ba64716679bac49adf2bfa74e8fe7409
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2020-08-02 00:38:28 -0300

    Tabs now have proper icon, name and routing

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -1,17 +1,58 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../home-tab/home-tab.module').then(m => m.HomeTabPageModule)
+          }
+        ]
+      },
+      {
+        path: 'events',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../events-tab/events-tab.module').then(m => m.EventsTabPageModule)
+          }
+        ]
+      },
+      {
+        path: 'notifications',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../notifications-tab/notifications-tab.module').then(m => m.NotificationsTabPageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/home',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
     path: '',
-    component: TabsPage
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class TabsPageRoutingModule {}
