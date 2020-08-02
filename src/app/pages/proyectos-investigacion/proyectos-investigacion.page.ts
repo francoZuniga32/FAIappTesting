2b1b11f9ee35afa35e7dba018f0395a16aef4084
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2020-08-02 00:36:45 -0300

    Proyectos investigacion now has design and content

--- src/app/pages/proyectos-investigacion/proyectos-investigacion.page.ts
+++ src/app/pages/proyectos-investigacion/proyectos-investigacion.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-proyectos-investigacion',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./proyectos-investigacion.page.scss'],
 })
 export class ProyectosInvestigacionPage implements OnInit {
+  proyecto = '';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  gotoAciis(): void {
+    this.router.navigateByUrl('/proyectos-investigacion/aciis');
+  }
+
+  gotoAeyws(): void {
+    this.router.navigateByUrl('/proyectos-investigacion/aeyws');
+  }
+
+  gotoAimfyae(): void {
+    this.router.navigateByUrl('/proyectos-investigacion/aimfyae');
+  }
+
+  gotoCacye(): void {
+    this.router.navigateByUrl('/proyectos-investigacion/cacye');
+  }
+
+  gotoCap(): void {
+    this.router.navigateByUrl('/proyectos-investigacion/cap');
+  }
+
+  gotoDsbr(): void {
+    this.router.navigateByUrl('/proyectos-investigacion/dsbr');
+  }
+
+  gotoIeyachysse(): void {
+    this.router.navigateByUrl('/proyectos-investigacion/ieyachysse');
+  }
+
+  gotoVdyrv(): void {
+    this.router.navigateByUrl('/proyectos-investigacion/vdyrv');
   }
 
 }
