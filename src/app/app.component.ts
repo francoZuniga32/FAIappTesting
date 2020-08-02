cdeb0dc117ea8700e1a3486046263858fd01a01d
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2020-08-02 00:47:40 -0300

    Added side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -6,14 +6,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  dark = false;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
   ) {
     this.initializeApp();
   }
@@ -24,4 +26,33 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
+  ngOnInit() {
+  }
+
+  // Modulo para abrir links en un navegador del dispositivo
+  close(url: string) {
+    window.open(url, '_blank');
+  }
+
+  // Modulos para cambiar de Modo Claro a Modo Oscuro
+  onClick(event) {
+    const systemDark = window.matchMedia('(prefers-color-scheme: dark)');
+    // tslint:disable-next-line: deprecation
+    systemDark.addListener(this.colorTest);
+    if (event.detail.checked) {
+      document.body.setAttribute('data-theme', 'dark');
+    } else {
+      document.body.setAttribute('data-theme', 'light');
+    }
+  }
+
+   colorTest(systemInitiatedDark) {
+    if (systemInitiatedDark.matches) {
+      document.body.setAttribute('data-theme', 'dark');
+    } else {
+      document.body.setAttribute('data-theme', 'light');
+    }
+  }
+
 }
